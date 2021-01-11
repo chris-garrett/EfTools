7f0c9b3fa70dc04e823c52dcf79ae4e66550ab90
Chris Garrett <chris@nesteggs.ca>
2021-01-11 15:31:39 -0700

    Everything in one project, using custom output paths ([obj|bin]/containers)

--- tool-create-migration.sh
+++ tool-create-migration.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#  --msbuildprojectextensionspath obj/container \
-
 pushd EfTools.Migrations
 
 DOTNET_RUNNING_IN_CONTAINER=true \
 ConnectionStrings__Sqlite="Data Source=eftools.sqlite" \
 \
 dotnet ef migrations add $1 \
+  --msbuildprojectextensionspath obj/container \
   --output-dir Migrations/Sqlite \
   --context SqliteDbContext \
   --project . \
