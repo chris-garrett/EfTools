5c264bceb31a4fef25967c5d1744516cda8523fa
Chris Garrett <chris@nesteggs.ca>
2021-01-11 15:46:09 -0700

    Separate projects, using custom output paths ([obj|bin]/containers)

--- tool-migrate.sh
+++ tool-migrate.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#  --msbuildprojectextensionspath obj/container \
-
 pushd EfTools.Data
 
 DOTNET_RUNNING_IN_CONTAINER=true \
 ConnectionStrings__Sqlite="Data Source=eftools.sqlite" \
 \
 dotnet ef database update \
+  --msbuildprojectextensionspath obj/container \
   --context SqliteDbContext \
   --project . \
   --startup-project ../EfTools.Migrations
